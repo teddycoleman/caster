dd787fd2e2dfa4e29498a4c22c8241b7ab69aa73
teddycoleman <teddy.f.coleman@gmail.com>
2018-11-25 19:09:43 -0800

    adds to form

--- eNotesFrontend/src/js/components/container/FormContainer.js
+++ eNotesFrontend/src/js/components/container/FormContainer.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import ReactDOM from "react-dom";
+import { Button } from 'react-bootstrap';
 import Input from "../presentational/Input";
 class FormContainer extends Component {
   constructor() {
@@ -7,6 +8,13 @@ class FormContainer extends Component {
     this.state = {
       first_name: "",
       last_name: "",
+      address: "",
+      city: "",
+      state: "",
+      country: "",
+      zipcode: "",
+      amount: 0,
+      rate_percent: 0,
     };
     this.handleChange = this.handleChange.bind(this);
   }
@@ -14,13 +22,14 @@ class FormContainer extends Component {
     this.setState({ [event.target.id]: event.target.value });
   }
   render() {
-    const { first_name, last_name } = this.state;
+    const { first_name, last_name, address, city, state, country, zipcode, amount, rate_percent } = this.state;
     return (
       <div>
         <h1>Create a new eNote</h1>
         <form id="enote-form">
+          <h2> Borrow Information </h2>
           <Input
-            text="First Name"
+            text="Borrower First Name"
             label="first_name"
             type="text"
             id="first_name"
@@ -28,14 +37,73 @@ class FormContainer extends Component {
             handleChange={this.handleChange}
           />
           <Input
-            text="Last Name"
+            text="Borrower Last Name"
             label="last_name"
             type="text"
             id="last_name"
             value={last_name}
             handleChange={this.handleChange}
           />
+          <Input
+            text="Address"
+            label="address"
+            type="text"
+            id="address"
+            value={address}
+            handleChange={this.handleChange}
+          />
+          <Input
+            text="City"
+            label="city"
+            type="text"
+            id="city"
+            value={city}
+            handleChange={this.handleChange}
+          />
+          <Input
+            text="State"
+            label="state"
+            type="text"
+            id="state"
+            value={state}
+            handleChange={this.handleChange}
+          />
+          <Input
+            text="Country"
+            label="country"
+            type="text"
+            id="country"
+            value={country}
+            handleChange={this.handleChange}
+          />
+          <Input
+            text="Zip Code"
+            label="zipcode"
+            type="text"
+            id="zipcode"
+            value={zipcode}
+            handleChange={this.handleChange}
+          />
+          <h2> Terms of Loan </h2>
+          <Input
+            text="Loan amount"
+            label="amount"
+            type="number"
+            id="amount"
+            value={amount}
+            handleChange={this.handleChange}
+          />          
+          <Input
+            text="Rate Percent"
+            label="rate_percent"
+            type="number"
+            id="rate_percent"
+            value={rate_percent}
+            handleChange={this.handleChange}
+          />
+          <Button type="submit">Create New eNote</Button>
         </form>
+        <br />
       </div>
     );
   }
