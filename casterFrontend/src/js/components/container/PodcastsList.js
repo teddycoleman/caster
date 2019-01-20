2844e5cb5a334a3ce975b428c5c0f193d3102012
teddycoleman <teddy.f.coleman@gmail.com>
2019-01-20 13:34:34 -0800

    adds embedded podcasts

--- casterFrontend/src/js/components/container/PodcastsList.js
+++ casterFrontend/src/js/components/container/PodcastsList.js
@@ -3,19 +3,19 @@ import PodcastEntry from "../presentational/PodcastEntry";
 
 const podcasts = [{
     id: 1,
-    title: 'First One!',
-    description: 'This is a podcast',
-    link: 'www.test.com',
+    title: 'Year of the Wallop - Reply All',
+    description: 'This is a podcast from Reply All.',
+    link: 'https://player.gimletmedia.com/8wh258',
   }, {
     id: 2,
-    title: 'Second One!',
-    description: 'This is another podcast',
-    link: 'www.test2.com',
+    title: 'Gary Brown v. Mayor of Detroit - Crimetown',
+    description: 'This is a podcast from Crimetown.',
+    link: 'https://open.spotify.com/embed?uri=spotify%3Aepisode%3A24G2Bz2naWQyGp6tN4cmCb',
   }, {
     id: 3,
-    title: 'Third One!',
-    description: 'This is a third podcast',
-    link: 'www.test3.com',
+    title: 'The Mystery of the Man Who Died Twice - Science Vs',
+    description: 'This is a podcast from Science Vs.',
+    link: 'https://player.gimletmedia.com/brhod5',
 }];
 
 function DisplayList(props) {
--- casterFrontend/src/js/components/presentational/PodcastEntry.js
+++ casterFrontend/src/js/components/presentational/PodcastEntry.js
@@ -1,10 +1,17 @@
 import React from "react";
 
+const playerIframe = {
+    width: '100%',
+    height: '130px',
+    borderRadius: '3px',
+    boxShadow: '0 0 25px 0',
+};
+
 const PodcastEntry = ({ title, description, link }) => (
   <div>
     <h2> {title} </h2>
     <p> {description} </p>
-    <p> {link} </p>
+    <iframe scrolling="no" frameBorder="no" style={playerIframe} src={link}></iframe>
   </div>
 );
 export default PodcastEntry;
