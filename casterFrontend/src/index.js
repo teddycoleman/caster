c1690a7298ad00fd6efb78eef9a48fb1fd3ffc09
teddycoleman <teddy.f.coleman@gmail.com>
2019-04-28 14:42:56 -0700

    reworks file structure

--- casterFrontend/src/index.js
+++
@@ -1,5 +0,0 @@
-import NavBar from "./js/components/container/NavBar";
-import About from "./js/components/container/About";
-import App from "./js/components/container/App";
-import Routes from "./js/components/Routes";
-import PodcastsList from "./js/components/container/PodcastsList";
