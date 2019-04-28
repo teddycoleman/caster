36bbb1000a019f47432e97c04e19ab8ee56c8d07
teddycoleman <teddy.f.coleman@gmail.com>
2019-04-28 15:04:24 -0700

    adds back the src folder... lol

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import NavBar from "./js/components/container/NavBar";
+import About from "./js/components/container/About";
+import App from "./js/components/container/App";
+import Routes from "./js/components/Routes";
+import PodcastsList from "./js/components/container/PodcastsList";
