ee1e340d86c5664be91c77438b7744f8da3e7654
teddycoleman <teddy.f.coleman@gmail.com>
2019-04-28 11:28:23 -0700

    updates how podcasts are stored

--- casterFrontend/src/js/components/container/PodcastsList.js
+++ casterFrontend/src/js/components/container/PodcastsList.js
@@ -1,31 +1,37 @@
 import React, { Component } from "react";
 import PodcastEntry from "../presentational/PodcastEntry";
 
-const podcasts = [{
-    id: 1,
-    title: 'Year of the Wallop - Reply All',
-    description: 'This is a podcast from Reply All.',
-    link: 'https://player.gimletmedia.com/8wh258',
-  }, {
-    id: 2,
-    title: 'Gary Brown v. Mayor of Detroit - Crimetown',
-    description: 'This is a podcast from Crimetown.',
-    link: 'https://open.spotify.com/embed?uri=spotify%3Aepisode%3A24G2Bz2naWQyGp6tN4cmCb',
-  }, {
-    id: 3,
-    title: 'The Mystery of the Man Who Died Twice - Science Vs',
-    description: 'This is a podcast from Science Vs.',
-    link: 'https://player.gimletmedia.com/brhod5',
-}];
+const podcastsByDate = require('../../../podcasts.json');
 
 function DisplayList(props) {
+  const podcastsForAllDatesArray = props.podcastsByDate;
+  const listItems = podcastsForAllDatesArray.map((podcastsByDate) => {
+    const date = podcastsByDate.date;
+    const podcasts = podcastsByDate.podcasts;
+    return (
+      <div>
+        <h1>{date}</h1>
+        <DisplayAllPodcastsForOneDate podcasts={podcasts}/>
+      </div>
+    );
+  })
+  return (
+    <div>
+      {listItems}
+    </div>
+  );
+};
+
+function DisplayAllPodcastsForOneDate(props) {
   const podcasts = props.podcasts;
   const listItems = podcasts.map((podcastData) =>
-    <PodcastEntry 
-      key={podcastData.id.toString()}
-      title={podcastData.title}
-      description={podcastData.description} 
-      link={podcastData.link}/>
+    <div>
+      <PodcastEntry 
+        key={podcastData.id.toString()}
+        title={podcastData.title}
+        description={podcastData.description} 
+        link={podcastData.link}/>
+    </div>
   );
   return (
     <div>
@@ -34,12 +40,12 @@ function DisplayList(props) {
   );
 };
 
+
 class PodcastsList extends Component {
   render() {
     return (
       <div>
-        <h1>January 13th, 2019</h1>
-        <DisplayList podcasts={podcasts}/>
+        <DisplayList podcastsByDate={podcastsByDate}/>
       </div>
     );
   }
