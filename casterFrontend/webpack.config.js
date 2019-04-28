f8bc570c811d0d50e0257e48f095b261d1f60141
teddycoleman <teddy.f.coleman@gmail.com>
2019-04-28 14:08:31 -0700

    updates webpack to be able to handle about us page properly

--- casterFrontend/webpack.config.js
+++ casterFrontend/webpack.config.js
@@ -1,28 +1,26 @@
+const path = require('path');
 const HtmlWebPackPlugin = require("html-webpack-plugin");
+
 module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'index_bundle.js',
+    publicPath: '/'
+  },
   module: {
     rules: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: "babel-loader"
-        }
-      },
-      {
-        test: /\.html$/,
-        use: [
-          {
-            loader: "html-loader"
-          }
-        ]
-      }
+      { test: /\.(js)$/, use: 'babel-loader' },
+      { test: /\.css$/, use: [ 'style-loader', 'css-loader' ]}
     ]
+  },
+  devServer: {
+    historyApiFallback: true,
   },
   plugins: [
     new HtmlWebPackPlugin({
       template: "./src/index.html",
-      filename: "./index.html"
+      filename: "./index.html",
     })
   ]
 };
