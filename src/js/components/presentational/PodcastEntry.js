4a63a81c382a003d4affe1e5d2a83033e021337f
teddycoleman <teddy.f.coleman@gmail.com>
2019-05-08 11:59:33 -0400

    removes unused line

--- src/js/components/presentational/PodcastEntry.js
+++ src/js/components/presentational/PodcastEntry.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-const PodcastEntry = ({ title, description, parent, link, url, parentLink, parentImage }) => (
+const PodcastEntry = ({ title, description, parent, url, parentLink, parentImage }) => (
   <div className="col-md-12 podcast-entry">
     <img className="col-md-3 no-left-padding" src={parentImage}></img>
     <h2> <a href={url} target="_blank">{title}</a> by <a href={parentLink} target="_blank">{parent}</a></h2>
